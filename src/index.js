7e343d6ee1887b7f73524c1e51401a1a26157b4e
AkatsukiDevs <58138168+AkatsukiDevs@users.noreply.github.com>
2022-12-13 22:25:26 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  /* eslint no-eval: 0 */
+
+  const [input, setInput] = useState("");
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
+    calcBtns.push(
+      <button
+        onClick={(e) => {
+          setInput(input + e.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {" "}
+        {item}
+      </button>
+    );
+  });
+
+  return (
+    <div className="wrapper">
+      {" "}
+      <div className="show-input">{input}</div>
+      <div className="digits flex">{calcBtns}</div>
+      <div className="modifiers subgrid">
+        {/* clear button */}
+        <button onClick={() => setInput("")} value="">
+          C
+        </button>
+        <button onClick={() => setInput("Hello!")} value="">
+          EN
+        </button>
+        <button onClick={() => setInput("Саламатсызбы!")} value="">
+          KG
+        </button>
+        <button onClick={() => setInput("Привет!")} value="">
+          RU
+        </button>
+
+        {/* clear all */}
+       
+      </div>
+      <div className="operations subgrid">
+        {/* add button */}
+        <button onClick={(e) => setInput(input + e.target.value)} value="+">
+          +
+        </button>
+
+        {/* minus btn */}
+        <button onClick={(e) => setInput(input + e.target.value)} value="-">
+          {" "}
+          -{" "}
+        </button>
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="*">
+          {" "}
+          *
+        </button>
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="/">
+          {" "}
+          /
+        </button>
+        {/* "=" btn */}
+        <button
+          onClick={(e) => {
+            try {
+              setInput(
+                String(eval(input)).length > 3 &&
+                  String(eval(input)).includes(".")
+                  ? String(eval(input).toFixed(4))
+                  : String(eval(input))
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
